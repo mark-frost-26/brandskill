bdc3c2779ff109426221e672d2836f24f12f7150
mark-frost-26 <savranskyboris@gmail.com>
2026-05-14 22:10:12 -0400

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: '**' },
+      { protocol: 'http', hostname: '**' },
+    ],
+  },
+  // Replit-friendly: longer timeouts for scraper
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'prisma'],
+  },
+}
+
+module.exports = nextConfig
